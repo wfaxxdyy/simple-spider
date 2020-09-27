827877e52b1263bf66d407cda28065cda4e4a362
wang fan <fan.wang02@weimob.com>
2020-09-27 17:03:03 +0800

    1.添加本地日志;2.并发下的list优化;3.爬虫安全:增加不同等待时间;4.code review;

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -13,3 +13,15 @@
          `day` datetime NOT NULL COMMENT '日期',
          PRIMARY KEY (`id`)
     ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='B站视频信息表';
+
+    CREATE TABLE `ss_video_extend` (
+        `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+        `bvid` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'bvid',
+        `video_id` bigint(20) NOT NULL COMMENT '视频id',
+        `like` bigint(20) NOT NULL COMMENT '点赞',
+        `coin` bigint(20) NOT NULL COMMENT '硬币',
+        `favorite` bigint(20) NOT NULL COMMENT '收藏',
+        `share` bigint(20) NOT NULL COMMENT '转发',
+        `danmaku` bigint(20) NOT NULL COMMENT '弹幕',
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='视频扩展表';
